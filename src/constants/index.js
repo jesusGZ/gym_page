f4fa5c1da2654fa98b8dde5c1365a8a631ad73a4
jesusGZ <j.gr.zurita@gmail.com>
2022-07-23 10:28:23 -0500

    constatnts

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const YOUTUBE_HOST = import.meta.env.VITE_APP_YOUTUBE_HOST;
+export const EXERCISE_HOST = import.meta.env.VITE_APP_EXERCISE_HOST;
+export const EXERCISE_API_KEY = import.meta.env.VITE_APP_EXERCISE_API_KEY;
+export const YOUTUBE_API_KEY = import.meta.env.VITE_APP_YOUTUBE_API_KEY;
